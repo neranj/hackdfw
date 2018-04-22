383bcce40a1fe2ffb2da8a975781ebf5d93b6d37
neranj <ponarasu1990@gmail.com>
2018-04-22 11:31:13 -0500

    swagger + main.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -5,14 +5,26 @@ import '../static/libs/bootstrap4/css/bootstrap.min.css'
 
 class Login extends Component{
 
-    handleClick(){
-        console.log('event clicked');
+    handleClick(e){
+        e.preventDefault();
+
+        axios.get('http://localhost:8080/users/login')
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+
+        return false;
     }
 
     render(){
+        const self = this;
+
         return (
             <div id="login-container">
-                <form className="form-signin">
+                <form className="form-signin" action="#" onSubmit={self.handleClick}>
                     <h2 className="form-signin-heading">Please sign in</h2>
                     <label htmlFor="inputEmail" className="sr-only">Email address</label>
                     <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
@@ -24,7 +36,8 @@ class Login extends Component{
                             <input type="checkbox" value="remember-me" /> Remember me
                         </label>
                     </div>
-                    <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={this.handleClick}>Sign in</button>
+                    <button className="btn btn-lg btn-primary btn-block"
+                            type="submit">Sign in</button>
                 </form>
 
             </div>
